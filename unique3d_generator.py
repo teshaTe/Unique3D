933e3458249ea6894935500b36d847e70db9df9d
teshaTe <tereshin.tesha@gmail.com>
2024-07-21 14:21:07 +0100

    [ADDED] env installation script

--- unique3d_generator.py
+++ unique3d_generator.py
@@ -8,7 +8,7 @@ from app.all_models import model_zoo
 
 
 if __name__ == '__main__':
-    image_name = "/home/tesha/Documents/Python/Unique3D/app/examples/bag.png"
+    image_name = "./app/examples/bag.png"
     remove_background = True
 
     model_zoo.init_models()
