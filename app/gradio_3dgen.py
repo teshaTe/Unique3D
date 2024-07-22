301a6262f8fb2c0cc16baa642081de1e6f9b6e1e
teshaTe <tereshin.tesha@gmail.com>
2024-07-22 11:14:44 +0100

    [UPDATED] added output path

--- app/gradio_3dgen.py
+++ app/gradio_3dgen.py
@@ -9,12 +9,12 @@ from scripts.refine_lr_to_sr import run_sr_fast
 from scripts.utils import save_glb_and_video
 from scripts.multiview_inference import geo_reconstruct
 
-def generate3dv2(preview_img, input_processing, seed, render_video=True, do_refine=True, expansion_weight=0.1, init_type="std"):
+def generate3dv2(preview_img, input_processing, seed, render_video=True, do_refine=True, expansion_weight=0.1, init_type="std", save_path=""):
     if preview_img is None:
         raise gr.Error("preview_img is none")
     if isinstance(preview_img, str):
         preview_img = Image.open(preview_img)
-    
+
     if preview_img.size[0] <= 512:
         preview_img = run_sr_fast([preview_img])[0]
     rgb_pils, front_pil = run_mvprediction(preview_img, remove_bg=input_processing, seed=int(seed)) # 6s
@@ -23,8 +23,8 @@ def generate3dv2(preview_img, input_processing, seed, render_video=True, do_refi
     vertices = vertices / 2 * 1.35
     vertices[..., [0, 2]] = - vertices[..., [0, 2]]
     new_meshes = Meshes(verts=[vertices], faces=new_meshes.faces_list(), textures=new_meshes.textures)
-    
-    ret_mesh, video = save_glb_and_video("/tmp/gradio/generated", new_meshes, with_timestamp=True, dist=3.5, fov_in_degrees=2 / 1.35, cam_type="ortho", export_video=render_video)
+
+    ret_mesh, video = save_glb_and_video(save_path, new_meshes, with_timestamp=True, dist=3.5, fov_in_degrees=2 / 1.35, cam_type="ortho", export_video=render_video)
     return ret_mesh, video
 
 #######################################
@@ -32,7 +32,7 @@ def create_ui(concurrency_id="wkl"):
     with gr.Row():
         with gr.Column(scale=2):
             input_image = gr.Image(type='pil', image_mode='RGBA', label='Frontview')
-            
+
             example_folder = os.path.join(os.path.dirname(__file__), "./examples")
             example_fns = sorted([os.path.join(example_folder, example) for example in os.listdir(example_folder)])
             gr.Examples(
@@ -42,13 +42,13 @@ def create_ui(concurrency_id="wkl"):
                 label='Examples (click one of the images below to start)',
                 examples_per_page=12
             )
-            
+
 
         with gr.Column(scale=3):
             # export mesh display
             output_mesh = gr.Model3D(value=None, label="Mesh Model", show_label=True, height=320)
             output_video = gr.Video(label="Preview", show_label=True, show_share_button=True, height=320, visible=False)
-            
+
             input_processing = gr.Checkbox(
                 value=True,
                 label='Remove Background',
@@ -60,7 +60,7 @@ def create_ui(concurrency_id="wkl"):
             setable_seed = gr.Slider(-1, 1000000000, -1, step=1, visible=True, label="Seed")
             render_video = gr.Checkbox(value=False, visible=False, label="generate video")
             fullrunv2_btn = gr.Button('Generate 3D', interactive=True)
-            
+
     fullrunv2_btn.click(
         fn = generate3dv2,
         inputs=[input_image, input_processing, setable_seed, render_video, do_refine, expansion_weight, init_type],
