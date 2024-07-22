36b2ca5e6ccf55f81047bc35c5031e4170e025e1
teshaTe <tereshin.tesha@gmail.com>
2024-07-22 11:08:45 +0100

    [UPDATED] small fix

--- mesh_reconstruction/recon.py
+++ mesh_reconstruction/recon.py
@@ -12,8 +12,8 @@ from scripts.utils import to_py3d_mesh, init_target
 def reconstruct_stage1(pils: List[Image.Image], steps=100, vertices=None, faces=None, start_edge_len=0.15, end_edge_len=0.005, decay=0.995, return_mesh=True, loss_expansion_weight=0.1, gain=0.1):
     vertices, faces = vertices.to("cuda"), faces.to("cuda")
     assert len(pils) == 4
-    mv,proj = make_star_cameras_orthographic(4, 1)
-    renderer = NormalsRenderer(mv,proj,list(pils[0].size))
+    mv, proj = make_star_cameras_orthographic(4, 1)
+    renderer = NormalsRenderer(mv,proj, list(pils[0].size))
     # cameras = make_star_cameras_orthographic_py3d([0, 270, 180, 90], device="cuda", focal=1., dist=4.0)
     # renderer = Pytorch3DNormalsRenderer(cameras, list(pils[0].size), device="cuda")
 
@@ -22,7 +22,7 @@ def reconstruct_stage1(pils: List[Image.Image], steps=100, vertices=None, faces=
     target_images = target_images[[0, 3, 2, 1]]
 
     # 2. init from coarse mesh
-    opt = MeshOptimizer(vertices,faces, local_edgelen=False, gain=gain, edge_len_lims=(end_edge_len, start_edge_len))
+    opt = MeshOptimizer(vertices, faces, local_edgelen=False, gain=gain, edge_len_lims=(end_edge_len, start_edge_len))
 
     vertices = opt.vertices
 
@@ -34,8 +34,8 @@ def reconstruct_stage1(pils: List[Image.Image], steps=100, vertices=None, faces=
         normals = calc_vertex_normals(vertices, faces)
         images = renderer.render(vertices, normals, faces)
 
-        # loss_expand = 0.5 * ((vertices+normals).detach() - vertices).pow(2).mean()
-        loss_expand = 0.5 * ((vertices+normals) - vertices).pow(2).mean()
+        loss_expand = 0.5 * ((vertices+normals).detach() - vertices).pow(2).mean()
+        # loss_expand = 0.5 * ((vertices+normals) - vertices).pow(2).mean()
 
         t_mask = images[..., -1] > 0.5
         loss_target_l2 = (images[t_mask] - target_images[t_mask]).abs().pow(2).mean()
